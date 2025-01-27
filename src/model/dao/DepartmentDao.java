6233633c361c9ca7769439867b8ec27e87a26c67
Hugohumbsen <hugosene@hotmail.com>
2025-01-27 15:19:15 -0300

    [200~SellerDaoJDBC and DaoFactory git commit -m SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,16 @@
 package model.dao;
 
-public interface DepartmentDao {
+import java.util.List;
 
+import model.entites.Department;
+
+public interface DepartmentDao {
+	
+	void insert (Department obj);
+	void update(Department obj);
+	void deleteByid(Integer id);
+	Department finById(Integer id);
+	List<Department> findAll();
+	
+	
 }
